c09d5a003bdd975a2504b0626c9d99844c3f37d9
M0hammed-Gamal <133115895+M0hammed-Gamal@users.noreply.github.com>
2024-10-23 01:04:55 +0300

    Update script.js
    add js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("CV Page Loaded for Mohamed Gamal");
+// Dark Mode Toggle
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
